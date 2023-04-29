38295d634df45eb2678d7654af988f6d1fe934f6
little_hobbit <rusikarm3@gmail.com>
2023-04-29 16:21:58 +0300

    setup client

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,8 @@
 #include <version_accessor.h>
 
 int main(int argc, char* argv[]) {
-
+  constexpr auto version = version::get_version();
+  std::cout << "Version is " << version.to_string() << std::endl;
+  std::cout << "Hello, World!" << std::endl;
+  std::cout << "Hello, GitHub Actions!" << std::endl;
 }
